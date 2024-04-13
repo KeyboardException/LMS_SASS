a78d3b8f8ebacefd1f6db6aa28d93682c96d7957
duonnn <72364556+kakashi250802@users.noreply.github.com>
2024-04-13 22:19:15 +0700

    fix quiz AttemptDetailModels

---
+++ Migrations/20240413151601_AddModels.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_SASS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuizAttemptDetails_QuizAttempts_QuizAttemptId",
+                table: "QuizAttemptDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuizAttemptDetails_QuizQuestions_QuestionId",
+                table: "QuizAttemptDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuizAttemptDetails_QuestionId",
+                table: "QuizAttemptDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuizAttemptDetails_QuizAttemptId",
+                table: "QuizAttemptDetails");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizAttemptDetails_QuestionId",
+                table: "QuizAttemptDetails",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizAttemptDetails_QuizAttemptId",
+                table: "QuizAttemptDetails",
+                column: "QuizAttemptId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuizAttemptDetails_QuizAttempts_QuizAttemptId",
+                table: "QuizAttemptDetails",
+                column: "QuizAttemptId",
+                principalTable: "QuizAttempts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuizAttemptDetails_QuizQuestions_QuestionId",
+                table: "QuizAttemptDetails",
+                column: "QuestionId",
+                principalTable: "QuizQuestions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Models/QuizAttemptDetailModel.cs
+++ Models/QuizAttemptDetailModel.cs
@@ -15,12 +15,8 @@ namespace LMS_SASS.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public required int Id { get; set; }
-        [ForeignKey(nameof(QuizAttempt))]
         public required int QuizAttemptId { get; set; }
-        public required QuizAttemptModel QuizAttempt { get; set; }
-        [ForeignKey(nameof(QuizQuestion))]
         public required int QuestionId { get; set; }
-        public required QuizQuestionModel QuizQuestion { get; set; }
         public required int Answered { get; set; }
         public required bool IsCorrect { get; set; }
     }
